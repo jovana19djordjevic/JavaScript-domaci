8edf994cf08707e3bbb0b93a3539e89f1f006b02
jovana19djordjevic <jovana19djordjevic@gmail.com>
2025-11-12 13:10:41 +0100

    Domaci-2

---
+++ Domaci 2/script.js
@@ -0,0 +1,29 @@
+let shop = {
+	name: "Fortuuna",
+	products: ["Mleko", "Brasno", "Voda", "Hleb"],
+	openingHours: 8,
+	closingHours: 16,
+};
+
+console.log(shop);
+
+//Vezba koje je ispod predavanja//
+
+let names = [];
+
+console.log(names);
+
+names.push("Marija", "Petar", "Jovan");
+console.log(names);
+
+let komponente = ["Monitor", "Tastatura", "Mis"];
+console.log(komponente);
+
+komponente.splice(0, 3);
+console.log(komponente);
+
+let books = ["Razgori sa Bogom1", "Razgovori sa Bogom2", "Razgovori sa bogom3"];
+console.log(books);
+
+books.unshift("Atomske navike");
+console.log(books);
